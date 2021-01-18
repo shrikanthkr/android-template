f6d3648ac57731e984b46e6a4931461798e3f765
shrikanthkr <shrikanth_kr@yahoo.com>
2021-01-18 19:48:27 +0530

    final commit for the assignment template

--- app/src/main/java/com/justshow/justshow/fragments/EnterUsernameFragment.kt
+++ app/src/main/java/com/justshow/justshow/fragments/EnterUsernameFragment.kt
@@ -1,24 +1,14 @@
 package com.justshow.justshow.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.justshow.app.R
 import com.justshow.app.databinding.FragmentEnterUserNameBinding
 import com.justshow.core.launch
 import dagger.android.support.DaggerFragment
 
-class EnterUsernameFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return FragmentEnterUserNameBinding.inflate(inflater, container, false).root
-
-    }
+class EnterUsernameFragment : Fragment(R.layout.fragment_enter_user_name) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
